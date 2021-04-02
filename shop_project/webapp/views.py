c0865662d25c7015ab07735d6ee6dd7d47275fa3
ormon <ormonkenenbaev@gmail.com>
2021-04-02 13:55:30 +0600

    detailview +

--- shop_project/webapp/views.py
+++ shop_project/webapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DeleteView
 from .models import Product
 
 # Create your views here.
@@ -10,3 +10,9 @@ class ProductListView(ListView):
     context_object_name = "products"
     model = Product
     paginate_by = 10
+
+
+class ProductDetailView(DeleteView):
+    model = Product
+    template_name = "product_detail.html"
+    context_object_name = "product"
