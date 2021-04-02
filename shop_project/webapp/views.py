70294545a552beccea3e62fd90dd0b9676168f1c
ormon <ormonkenenbaev@gmail.com>
2021-04-02 14:33:31 +0600

    CreateView+

--- shop_project/webapp/views.py
+++ shop_project/webapp/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DeleteView
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DeleteView, CreateView
 from .models import Product
+from .forms import ProductForm
 
 # Create your views here.
 
@@ -16,3 +17,15 @@ class ProductDetailView(DeleteView):
     model = Product
     template_name = "product_detail.html"
     context_object_name = "product"
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    template_name = 'product_form.html'
+    form_class = ProductForm
+
+    def form_valid(self, form):
+        print(form)
+        product = form.save(commit=False)
+        product.save()
+        return redirect('product_list')
